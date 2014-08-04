d0d0ca7387b898a8908e7a7080aaa8ed7363a39c
robertlyc <merlinlyc@gmail.com>
2014-08-04 17:00:46 +0800

    01-Table View

--- Checklist/ChecklistViewController.h
+++ Checklist/ChecklistViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ChecklistViewController : UIViewController
+@interface ChecklistViewController : UITableViewController
 
 @end
--- Checklist/ChecklistViewController.m
+++ Checklist/ChecklistViewController.m
@@ -14,6 +14,40 @@
 
 @implementation ChecklistViewController
 
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 100;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    UILabel *label = (UILabel *)[cell viewWithTag:1000];
+    
+    if (indexPath.row %5 == 0) {
+        label.text = @"Walk the dog";
+    } else if (indexPath.row %5 == 1) {
+        label.text = @"Brush my teeth";
+    } else if (indexPath.row %5 == 2) {
+        label.text = @"Learn iOS development";
+    } else if (indexPath.row %5 == 3) {
+        label.text = @"Soccer practice";
+    } else if (indexPath.row %5 == 4) {
+        label.text = @"Eat ice cream";
+    }
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if (cell.accessoryType == UITableViewCellAccessoryNone) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
