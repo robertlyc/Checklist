731b2b15d11258598ea202a4c6f5a61e3eef22c0
robertlyc <merlinlyc@gmail.com>
2014-08-21 14:31:33 +0800

    cancel notification when date changed

--- Checklist/ChecklistItem.m
+++ Checklist/ChecklistItem.m
@@ -42,6 +42,13 @@
 }
 
 - (void)scheduleNotification {
+    UILocalNotification *existingNotification = [self notificationForThisItem];
+    if (existingNotification != nil) {
+        NSLog(@"Found an existing notification %@", existingNotification);
+        [[UIApplication sharedApplication] cancelLocalNotification:existingNotification];
+    }
+    
+    
     if (self.shouldRemind && [self.dueDate compare:[NSDate date]] != NSOrderedAscending) {
         UILocalNotification *localNotification = [[UILocalNotification alloc] init];
         localNotification.fireDate = self.dueDate;
@@ -53,6 +60,26 @@
         [[UIApplication sharedApplication] scheduleLocalNotification:localNotification];
         
         NSLog(@"Scheduled notification %@ for itemId %d", localNotification, self.itemId);
+    }
+}
+
+- (UILocalNotification *)notificationForThisItem {
+    NSArray *allNotifications = [[UIApplication sharedApplication] scheduledLocalNotifications];
+    
+    for (UILocalNotification *notification in allNotifications) {
+        NSNumber *number = [notification.userInfo objectForKey:@"ItemID"];
+        if (number != nil && [number integerValue] == self.itemId) {
+            return notification;
+        }
+    }
+    return nil;
+}
+
+- (void)dealloc {
+    UILocalNotification *existingNotification = [self notificationForThisItem];
+    if (existingNotification != nil) {
+        NSLog(@"Removing existing notification %@", existingNotification);
+        [[UIApplication sharedApplication] cancelLocalNotification:existingNotification];
     }
 }
 
